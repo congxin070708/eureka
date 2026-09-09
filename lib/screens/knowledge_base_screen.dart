e4d651411890114c9bded838205948547e92f5d2
daniyous <z38373653@gmail.com>
2026-09-09 21:34:22 +0800

    fix: knowledge_base_screen 补充 dart:typed_data import

--- lib/screens/knowledge_base_screen.dart
+++ lib/screens/knowledge_base_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:file_picker/file_picker.dart';
